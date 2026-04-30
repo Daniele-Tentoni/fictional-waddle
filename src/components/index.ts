ab0c621d90d51f5d9b85df8b647e9a4699c52fca
Daniele Tentoni <daniele.tentoni@horsa.it>
2026-04-30 15:40:52 +0200

    Remove components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as BButton } from './button/Button.vue';
-export { default as BContainer } from './grid/Container.vue';
+/*export { default as BContainer } from './grid/Container.vue';
 export { default as BGrid } from './grid/Grid.vue';
 export { default as BItem } from './grid/Item.vue';
+*/
